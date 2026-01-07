2391c2b981f568d7131471a76111152a6e1df56a
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-01-07 15:36:14 +0100

    small change in traceback

--- TypedUnit/__init__.py
+++ TypedUnit/__init__.py
@@ -4,7 +4,6 @@ try:
 except ImportError:
     __version__ = "0.0.0"
 
-
-from .units import *
-from .helper import validate_units
-from .units import ureg, Quantity
+from TypedUnit.units import *
+from TypedUnit.helper import validate_units
+from TypedUnit.units import ureg, Quantity
