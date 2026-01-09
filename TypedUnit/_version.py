163e0180a5482012a6a0163839d583a08d8e4329
MartinPdeS <martin.poinsinet.de.sivry@gmail.com>
2026-01-09 15:59:25 +0100

    update

--- TypedUnit/_version.py
+++ TypedUnit/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.0.8"
-__version_tuple__ = version_tuple = (0, 0, 8)
+__version__ = version = "0.0.9"
+__version_tuple__ = version_tuple = (0, 0, 9)
 
-__commit_id__ = commit_id = "gb45b0bac8"
+__commit_id__ = commit_id = "g2391c2b98"
